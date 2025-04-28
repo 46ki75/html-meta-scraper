f40b6c935632b7331bcbf0f635216d10e643a5ee
Chomolungma Shirayuki <shirayuki@46ki75.com>
2025-04-28 04:08:01 +0000

    feat: Update repository description and add LICENSE file

--- terraform/github/repository.tf
+++ terraform/github/repository.tf
@@ -1,6 +1,6 @@
 resource "github_repository" "repository" {
   name        = local.repository
-  description = "Template Repository"
+  description = "Scrape and extract metadata like title, description, images, and favicon from HTML documents."
 
   has_downloads        = false
   has_discussions      = false
